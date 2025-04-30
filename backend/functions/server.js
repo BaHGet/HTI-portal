228392500579d06e5adf32f908c5df71dd50b6cd
BaHGet <abosama150@gmail.com>
2025-04-30 19:00:20 +0300

    create backend project

---
+++ backend/functions/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const serverless = require('serverless-http');
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.send("hi")
+})
+
+app.listen(3000)
+
+// module.exports.handler = serverless(app);
