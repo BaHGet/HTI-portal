1189872f3ef6d6cc3949dff4c270814198f68abf
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2025-06-27 01:03:34 +0300

    adds routes of admin, addUser pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import Login from './Pages/Login.jsx' 
 import './App.css';
+import Admin from './Pages/admin.jsx';
+import AddUser from './Pages/addUser.jsx';
 
 const App = () => {
 
   return (
     <div>
       <Routes>
-        <Route path="/HTI-portal/" element={<Login/>}/>
+        <Route path="/HTI-portal/login" element={<Login/>}/>
+        <Route path="/HTI-portal/admin" element={<Admin/>}/>
+        <Route path="/HTI-portal/admin/adduser" element={<AddUser/>}/>
       </Routes>
     </div>
   );
