8d5f8ed0fb88958a6d9e579f66cadbdf94fc3db3
MohamedAdelElwekel <muhammedelwekel@gmail.com>
2025-07-22 00:12:56 +0300

    UserValidatoion layer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,14 @@ const express = require('express');
 const {getAllUsers, addUser, getUser} = require('../controller/user');
 const { VerifyToken } = require('../middlewares/authMiddleware');
 
+const {
+  getUserValidator,
+  addUserValidator
+} = require('../validations/userValidation')
 const userRouter = express.Router();
 
 userRouter.get('/getallusers', getAllUsers)
-userRouter.get('/getuser', VerifyToken, getUser)
-userRouter.post('/adduser', addUser)
+userRouter.get('/getuser', getUserValidator,VerifyToken, getUser)
+userRouter.post('/adduser', addUserValidator,addUser)
 
 module.exports = {userRouter}; 
--- backend/validations/userValidation.js
+++ backend/validations/userValidation.js
@@ -1,4 +1,50 @@
+const  check  = require('express-validator');
+const slugify = require('slugify');
+const validatorMiddleware = require ('../middlewares/validatorMiddleware')
+const User = require('../models/users');
 
 
 
+exports.getUserValidator=[
+  // 1- rules
+  check('email')
+  .notEmpty().withMessage('Email is required')
+  .isEmail().withMessage('Invalid email format'),
+  // 2-middleware
+  validatorMiddleware
+]
+
+exports.addUserValidator=[
+  check('name').notEmpty().withMessage('User required')
+  .isLength({min:3}).withMessage('Name is too short')
+  .custom( (val, { req }) => {
+    req.body.slug = slugify(val);
+    return true;
+  }),
+  check('email')
+  .notEmpty().withMessage('Email is required')
+  .isEmail().withMessage('Invalid email format')
+ .custom(async (val) => {
+    const user = await User.findOne({ email: val });
+    if (user) {
+      throw new Error('Email already in use');
+    }
+  }),
+  check('password')
+  .notEmpty().withMessage('Password is required')
+  .isLength({min:6}).withMessage('Password must be at least 6 characters long')
+  .custom((val, { req }) => {
+    if (val !== req.body.passwordConfirm) {
+      throw new Error('PasswordConfirmation do not match');
+    }
+    return true;
+  }),
+
+  check('passwordConfirm').notEmpty().withMessage('Password confirmation is required'),
+
+  check('role').optional().isIn(["student", "staff", "admin", "Graduated"]).withMessage('Role must be either user or admin'),
+
+  check('phone').isMobilePhone("ar-EG").withMessage('Invalid phone number'),
+  validatorMiddleware
+]
 
