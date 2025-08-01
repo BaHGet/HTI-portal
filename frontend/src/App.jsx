f7588570c450484177b90b6816604a2d4b1dff83
amdhazm <amdhazm0@gmail.com>
2025-08-01 15:09:30 +0300

    update the path of bais to be from /HTI-PORTAL/ to be /, and update the route of login page to be start from /auth/login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import Login from './Pages/Login.jsx' 
 import './App.css';
+import NewLogin from './Pages/NewLogin.jsx';
 
 const App = () => {
 
   return (
     <div>
       <Routes>
-        <Route path="/HTI-portal/" element={<Login/>}/>
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/new-login" element={<NewLogin />} />
       </Routes>
     </div>
   );
