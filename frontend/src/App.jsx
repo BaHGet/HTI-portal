18e4b3e03838affc242b55d8e23e094fbceb6de7
amdhazm <amdhazm0@gmail.com>
2025-08-01 22:33:49 +0300

    Add the NewLogin Page with it all needed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
-import Login from './Pages/Login.jsx' 
-import './App.css';
-import NewLogin from './Pages/NewLogin.jsx';
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login.jsx";
+import "./index.css";
+import NewLogin from "./Pages/NewLogin.jsx";
 
 const App = () => {
-
   return (
-    <div>
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-100">
       <Routes>
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/new-login" element={<NewLogin />} />
