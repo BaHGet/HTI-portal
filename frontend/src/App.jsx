659fa2059c13e989c674626829aad268cd64f3d1
amdhazm <amdhazm0@gmail.com>
2025-08-01 23:04:38 +0300

    Create InternalServerErrorPage 500

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login.jsx";
 import "./index.css";
-import NewLogin from "./Pages/NewLogin.jsx";
 import ForgotPassword from "./Pages/ForgotPassword.jsx";
 import NotFoundPage from "./Pages/NotFoundPage.jsx";
+import InternalServerErrorPage from "./Pages/InternalServerErrorPage.jsx";
 
 const App = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-100">
       <Routes>
         <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/new-login" element={<NewLogin />} />
         <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+        <Route path="/500" element={<InternalServerErrorPage />} />
+        
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
