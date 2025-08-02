6dbcd3ec90966e9ae2824705e366b15858af2d06
BaHGet <abosama150@gmail.com>
2025-08-02 03:16:58 +0300

    Update routing in App.jsx to include Dashboard component and modify login path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login.jsx";
+import Dashboard from "./Pages/Dashboard.jsx";
 import "./index.css";
 import ForgotPassword from "./Pages/ForgotPassword.jsx";
 import NotFoundPage from "./Pages/NotFoundPage.jsx";
@@ -9,7 +10,8 @@ const App = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-100">
       <Routes>
-        <Route path="/auth/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/auth/forgot-password" element={<ForgotPassword />} />
         <Route path="/500" element={<InternalServerErrorPage />} />
         
