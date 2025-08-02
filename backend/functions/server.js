0c01294a1f58d8f1ebde3a2f4ab5d85573c61ef0
BaHGet <abosama150@gmail.com>
2025-08-02 03:15:51 +0300

    Refactor server.js to include CORS middleware and improve logging format

--- backend/functions/server.js
+++ backend/functions/server.js
@@ -1,28 +1,37 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
-const morgan = require('morgan');
-const logger = require('../utils/logger');
-const cookieParser = require('cookie-parser');
-const globalError = require('../middlewares/apiMiddleware')
-
-
+const morgan = require("morgan");
+const logger = require("../utils/logger");
+const cookieParser = require("cookie-parser");
+const globalError = require("../middlewares/apiMiddleware");
 
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "token"],
+    exposedHeaders: ["token"],
+  })
+);
 
 // Use morgan middleware to log HTTP requests
 // Custom token for req.params
-morgan.token('params', (req) => JSON.stringify(req.params));
+morgan.token("params", (req) => JSON.stringify(req.params));
 
 // Define custom morgan token to log body (with password redacted)
-morgan.token('body', (req) => {
+morgan.token("body", (req) => {
   const clone = { ...req.body };
 
   // Redact password fields
   for (const key in clone) {
-    if (key.toLowerCase().includes('password')) {
-      clone[key] = '[REDACTED]';
+    if (key.toLowerCase().includes("password")) {
+      clone[key] = "[REDACTED]";
     }
   }
 
@@ -35,15 +44,19 @@ const customFormat = (tokens, req, res) => {
     tokens.method(req, res),
     tokens.url(req, res),
     tokens.status(req, res),
-    '- Params:', tokens.params(req, res),
-    '- Body:', tokens.body(req, res)
-  ].join(' ');
+    "- Params:",
+    tokens.params(req, res),
+    "- Body:",
+    tokens.body(req, res),
+  ].join(" ");
 };
-app.use(morgan(customFormat, {
-  stream: {
-    write: message => logger.info(message.trim())
-  }
-}));
+app.use(
+  morgan(customFormat, {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  })
+);
 
 app.use(cookieParser());
 
@@ -56,15 +69,13 @@ app.use("/api/auth", authRouter);
 const { userRouter } = require("../routes/user");
 app.use("/api/user", userRouter);
 
-
-app.all("/{*any}", (req,res,next)=>{
-  next(new ApiError(`Can't find this URL: ${req.originalUrl}`,400))
-})
+app.all("/{*any}", (req, res, next) => {
+  next(new ApiError(`Can't find this URL: ${req.originalUrl}`, 400));
+});
 
 app.get("/", (req, res) => {
   res.send("hi");
 });
-
 
 // Global error handling middleware for express
 app.use(globalError);
