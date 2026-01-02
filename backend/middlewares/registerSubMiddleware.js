b2e8ca3ec2ca7d0edac5720053694f91affcd603
MohamedAdelElwekel <muhammedelwekel@gmail.com>
2026-01-02 02:06:37 +0200

    Apply Caching

--- backend/middlewares/registerSubMiddleware.js
+++ backend/middlewares/registerSubMiddleware.js
@@ -2,8 +2,18 @@ const { Op } = require('sequelize');
 const asyncHandler = require('express-async-handler');
 const db = require('../models');
 const ApiError = require('../utils/apiError');
+const redisClient = require('../utils/redisClient');
+
 
 exports.setCurrentSemester = asyncHandler(async (req, res, next) => {
+  const cacheKey = 'global:currentSemester';
+  let cachedSemester = await redisClient.get(cacheKey);
+
+  if (cachedSemester) {
+    req.currentSemester = JSON.parse(cachedSemester);
+    return next();
+  }
+
   const today = new Date();
 
   const currentSemester = await db.Semester.findOne({
@@ -21,11 +31,20 @@ exports.setCurrentSemester = asyncHandler(async (req, res, next) => {
   if (!currentSemester) {
     return next(new ApiError('No active semester found. Registration might be closed.', 400));
   }
+
+  await redisClient.setEx(cacheKey, 60 , JSON.stringify(currentSemester));
   req.currentSemester = currentSemester;
   next();
 });
 
 exports.getStudent = asyncHandler(async(req,res,next)=>{
+  const studentCacheKey = `student:profile:user:${req.user.UserID}`;
+  let cachedStudent = await redisClient.get(studentCacheKey);
+
+  if (cachedStudent) {
+    req.student = JSON.parse(cachedStudent);
+    return next();
+  }
   const student = await db.Student.findOne({
     where: { UserID: req.user.UserID },
   });
@@ -34,6 +53,7 @@ exports.getStudent = asyncHandler(async(req,res,next)=>{
     return next(new ApiError('Forbidden: This user does not have a student profile.', 403));
   }
 
+  await redisClient.setEx(studentCacheKey, 60 , JSON.stringify(student));
   req.student = student;
   next();
 })
